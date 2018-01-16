0a26f53648deec0679506844763a03a8ffbaf2ad
gaosx <gaosx@cecurs.com>
2018-01-16 14:30:07 +0800

    add controller case.change port

--- sc02/src/main/java/cn/xz/ctrl/HelloCtrl.java
+++ sc02/src/main/java/cn/xz/ctrl/HelloCtrl.java
@@ -4,14 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloCtrl {
     @RequestMapping("")
-    public Map<String, String> hello() {
+    public Map<String, String> hello(@RequestParam(value = "name", required = true) String name) {
         Map<String, String> map = new HashMap<>();
+        map.put("name", name);
         map.put("time", "" + System.currentTimeMillis());
         return map;
     }
