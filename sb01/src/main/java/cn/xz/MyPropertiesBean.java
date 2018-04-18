932725bee48f793d834fd7f1e6eba592bd0b1974
g <g@cecurs.com>
2018-04-18 16:11:29 +0800

    @PropertySource和@ConfigurationProperties配合使用直接生成配置类

---
+++ sb01/src/main/java/cn/xz/MyPropertiesBean.java
@@ -0,0 +1,31 @@
+package cn.xz;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource(value = { "classpath:my.properties" })
+@ConfigurationProperties(prefix = "my")
+public class MyPropertiesBean {
+
+    private String config1;
+    private String config2;
+
+    public String getConfig1() {
+        return config1;
+    }
+
+    public void setConfig1(String config1) {
+        this.config1 = config1;
+    }
+
+    public String getConfig2() {
+        return config2;
+    }
+
+    public void setConfig2(String config2) {
+        this.config2 = config2;
+    }
+
+}
