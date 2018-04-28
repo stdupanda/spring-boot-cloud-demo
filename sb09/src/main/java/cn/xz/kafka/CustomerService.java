8bf6d51ffcea87946b8b687ac09f4bb10b07a449
gaosx <gaosx@cecurs.com>
2018-04-28 16:11:34 +0800

    update kafka intergration

--- sb09/src/main/java/cn/xz/kafka/CustomerService.java
+++ sb09/src/main/java/cn/xz/kafka/CustomerService.java
@@ -1,5 +1,6 @@
 package cn.xz.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,8 +11,8 @@ public class CustomerService {
 
     private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
 
-    @KafkaListener(topics= {"test_topic"})
-    public void recv(String msg) {
-        log.debug("recv msg: {}", msg);
+    @KafkaListener(topics = "test_topic")
+    public void recv(ConsumerRecord<?, ?> msg) {
+        log.debug("recv msg with topic: {}, key: {}, value: {}.", msg.topic(), msg.key(), msg.value());
     }
 }
--- sb09/src/main/java/cn/xz/kafka/ScheduleSend.java
+++ sb09/src/main/java/cn/xz/kafka/ScheduleSend.java
@@ -1,5 +1,7 @@
 package cn.xz.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,12 +10,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduleSend {
 
+    private static final Logger log = LoggerFactory.getLogger(ScheduleSend.class);
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @Scheduled(cron = "0/5 * * * * ?")
+    @Scheduled(fixedDelay = 100)
     public void send() {
-        kafkaTemplate.send("test_topic", "" + System.nanoTime()).addCallback(o -> System.out.println("send-消息发送成功"),
-                throwable -> System.out.println("消息发送失败"));
+        String topic = "test_topic";
+        String key = "test_key";
+        String data = "" + System.nanoTime();
+        kafkaTemplate.send(topic, key, data).addCallback(o -> log.debug("send-消息发送成功"),
+                throwable -> log.error("消息发送失败"));
+        log.debug("sent data: {}", data);
     }
 }
